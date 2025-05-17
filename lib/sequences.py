dad6dcc1fccf9e9d69d3da2f6016c63b1d3481f5
Bildad Ereggae <bildadereggae@gmail.com>
2025-05-17 13:59:04 +0300

    sequence lab done

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fib_list = []
+    if length == 0:
+        print (fib_list)
+    elif length == 1:
+        fib_list = [0]
+        print(fib_list)
+    else:
+        fib_list = [0, 1]  
+        for i in range(2, length):
+            fib_list.append(fib_list[i-1] + fib_list[i-2])
+        print(fib_list)
+    
+
+print(print_fibonacci)
